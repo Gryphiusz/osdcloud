7ef03674bf18d7764a6255c68ea81d2877d01e8c
Gryphiusz <131449709+Gryphiusz@users.noreply.github.com>
2024-10-16 15:18:33 +0200

    Create osdcloud.ps1

---
+++ osdcloud.ps1
@@ -0,0 +1,50 @@
+Write-Host -ForegroundColor Green "Starting Smart2Cloud ZTI"
+
+Start-Sleep -Seconds 2
+
+net use Z: \\WIN-T7I96T9MLSS\pi
+Write-Host -ForegroundColor Green "Mapping OSDCloud Network Folder"
+
+$imagename = 'install.wim'
+$imagefileitem = Find-OSDCloudFile -Name $imagename
+
+Start-Sleep -Seconds 5
+
+#Variables to define the Windows OS / Edition etc to be applied during OSDCloud
+$OSName = 'Windows 11 24H2 x64'
+$OSEdition = 'Pro'
+$OSActivation = 'Retail'
+$OSLanguage = 'nl-nl'
+
+#Set OSDCloud Vars
+$Global:MyOSDCloud = [ordered]@{
+    Restart = [bool]$False
+    RecoveryPartition = [bool]$true
+    OEMActivation = [bool]$True
+    WindowsUpdate = [bool]$true
+    WindowsUpdateDrivers = [bool]$true
+    WindowsDefenderUpdate = [bool]$true
+    SetTimeZone = [bool]$true
+    ClearDiskConfirm = [bool]$False
+    ShutdownSetupComplete = [bool]$false
+    SyncMSUpCatDriverUSB = [bool]$true
+    CheckSHA1 = [bool]$true
+    ImageFileItem = $imagefileitem
+}
+
+#Launch OSDCloud
+Write-Host "Starting OSDCloud" -ForegroundColor Green
+write-host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage"
+
+Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage
+
+
+#Start-OSDCloud -FindImageFile
+
+#Restart from WinPE
+
+Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+
+Start-Sleep -Seconds 20
+
+wpeutil reboot
